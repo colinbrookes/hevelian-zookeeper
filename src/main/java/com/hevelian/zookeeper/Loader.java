695dba2463b8054871e9c926d6fe349cca9320c1
Hevelian <colin@4tune.net>
2017-05-03 09:16:16 +0200

    initial.

---
+++ src/main/java/com/hevelian/zookeeper/Loader.java
@@ -0,0 +1,16 @@
+package com.hevelian.zookeeper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.zookeeper.server.ZooKeeperServer;
+
+public class Loader extends HttpServlet {
+
+	private ZooKeeperServer server;
+	
+	@Override
+	public void init() throws ServletException {
+	}
+	
+}
